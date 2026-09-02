fbf9a45766dfb79ce86516ab81a9a4855d773219
fftanmay36-coder <fftanmay36@gmail.com>
2026-09-02 19:12:25 +0530

    feat: update LeetCode 1. Two Sum solution (cpp)

--- problems/two-sum/solution.cpp
+++ problems/two-sum/solution.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        map<int , int >mpp;
         int n = nums.size();
-        for (int i = 0 ; i<n ; i++) {
-            for (int j = i+1 ; j<n ; j++){
-                if(nums[i] + nums[j] == target) {
-                  return {i , j};
-                }
+        for (int i = 0 ; i<n ; i++){
+            int y = target - nums[i];
+            if (mpp.find(y) != mpp.end()) {
+                return {mpp[y] , i};
             }
+            mpp[nums[i]] = i;
         }
         return {} ;
-    }
+     }
 };
